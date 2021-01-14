c6b4079fa1a186db2696d90d6726bd9b605fc1f9
Punane Madu <kaldt.slange@gmail.com>
2021-01-14 06:09:03 -0600

    2101140608 autocomplete works and it is clean

--- src/main/java/com/tts/mapsapp/model/AutocompleteLocation.java
+++ src/main/java/com/tts/mapsapp/model/AutocompleteLocation.java
@@ -45,39 +45,5 @@ public class AutocompleteLocation {
 		this.postal_code = "";
 	}
 	
-//	public String getCity() {
-//		return city;
-//	}
-//	public void setCity(String city) {
-//		this.city = city;
-//	}
-//	public String getState() {
-//		return state;
-//	}
-//	public void setState(String state) {
-//		this.state = state;
-//	}
-//	public String getLat() {
-//		return lat;
-//	}
-//	public void setLat(String lat) {
-//		this.lat = lat;
-//	}
-//	public String getLng() {
-//		return lng;
-//	}
-//	public void setLng(String lng) {
-//		this.lng = lng;
-//	}
-//	
-//	
-//	@Override
-//	public String toString() {
-//		return "Location [city=" + city + ", state=" + state + ", lat=" + lat + ", lng=" + lng + "]";
-//	} 
-	
-	
-	
-	
 
 }
